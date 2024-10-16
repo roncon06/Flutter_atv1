db21b40043a036fa60d670f6a38000b3721abde6
Roncon <lucasroncon16@gmail.com>
2024-10-15 22:17:33 -0300

    add formulario

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,74 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/api/cliente_api.dart';
+import 'package:mobile/screen/list.dart';
 import 'package:mobile/service/transacao_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BancoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class BancoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aplicação Bancária',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      var api = transacaoService();
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: ListaTransacoes(), // Inicializando com a tela de listagem
     );
   }
 }
--- mobile/lib/service/abstract_service.dart
+++ mobile/lib/service/abstract_service.dart
@@ -7,45 +7,43 @@ abstract class AbstractService {
 
   String recurso();
 
-  Future<void> getAll() async {
-    var response = await http.get(Uri.parse("$url/${recurso()}"));
-    print(response.body);
-  }
-
   Future<void> getById(int id) async {
     var response = http.get(Uri.parse("$url/${recurso()}"));
 
     print((await response).body);
   }
 
-  Future<void> createTransacoes(Object body) async {
-    var response = http.post(
-      Uri.parse("$url/${recurso()}"),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode({"id": 3, "valor": 500, "tipo": "credito"}),
-    );
-    print((await response).body);
+  Future<List<Map<String, dynamic>>> getAll() async {
+    var response = await http.get(Uri.parse("$url/${recurso()}"));
+    return List<Map<String, dynamic>>.from(jsonDecode(response.body));
   }
 
-  Future<void> deleteTransacoes(int id) async {
-    var response = http.delete(
+  Future<void> create(Map<String, dynamic> body) async {
+    await http.post(
       Uri.parse("$url/${recurso()}"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
+      body: jsonEncode(body),
     );
-    print((await response).body);
   }
 
-  Future<void> putTransacoes(int id, Object body) async {
-    var response = http.put(
-      Uri.parse("$url/${recurso()}"),
+  Future<void> update(int id, Map<String, dynamic> body) async {
+    await http.put(
+      Uri.parse("$url/${recurso()}/$id"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(body),
+    );
+  }
+
+  Future<void> delete(int id) async {
+    await http.delete(
+      Uri.parse("$url/${recurso()}/$id"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
     );
-    print((await response).body);
   }
 }
